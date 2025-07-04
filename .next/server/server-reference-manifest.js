cdfb384bfe826bee91c375c21397b97227a91711
Riddhi Patel <rmaptel9124@gmail.com>
2025-07-04 18:38:53 +0530

    Final cleanup before rebase

--- .next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"RBRta9CT5USup+7Om5qUBcjBw5Tm1JRf+aYtKMOIcsY=\"\n}"
--- .next/static/chunks/src_app_about_page_1f0f2f98.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/_5940c50f._.js"
-],
-    source: "dynamic"
-});
